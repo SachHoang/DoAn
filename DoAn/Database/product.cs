785b3f6aa3239094d13fb2d58ece1a16a1896716
nguyenchinhtan2003@gmail.com <nguyenchinhtan2003@gmail.com>
2023-10-26 12:48:18 +0700

    b

--- DoAn/Database/product.cs
+++ DoAn/Database/product.cs
@@ -9,12 +9,6 @@ namespace DoAn.Database
     [Table("Product")]
     public partial class Product
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Product()
-        {
-            tblDetails = new HashSet<tblDetail>();
-        }
-
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ProductID { get; set; }
 
@@ -26,12 +20,6 @@ namespace DoAn.Database
 
         public int catID { get; set; }
 
-        [Column(TypeName = "image")]
-        public byte[] pImage { get; set; }
-
         public virtual category category { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<tblDetail> tblDetails { get; set; }
     }
 }
